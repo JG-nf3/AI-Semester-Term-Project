5532ce9e6e4b530ffddd14173ce0f650fcec329e
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2020-11-07 15:23:55 -0500

    some major ui refactors & gitignore

--- AI_code/src/UI.java
+++ AI_code/src/UI.java
@@ -66,43 +66,32 @@ public class UI  extends JFrame {
 
         dir[0] = new JButton("Up");
         dir[0].setBounds(875, 240, 200, 40);
-        dir[0].setFont(new Font(dir[0].getFont().getName(), dir[0].getFont().getStyle(), 28));
-        add(dir[0]);
 
         dir[1] = new JButton("Down");
         dir[1].setBounds(875, 360, 200, 40);
-        dir[1].setFont(new Font(dir[1].getFont().getName(), dir[1].getFont().getStyle(), 28));
-        add(dir[1]);
 
         dir[2] = new JButton("Left");
         dir[2].setBounds(650, 300, 200, 40);
-        dir[2].setFont(new Font(dir[2].getFont().getName(), dir[2].getFont().getStyle(), 28));
-        add(dir[2]);
 
         dir[3] = new JButton("Right");
         dir[3].setBounds(1100, 300, 200, 40);
-        dir[3].setFont(new Font(dir[3].getFont().getName(), dir[3].getFont().getStyle(), 28));
-        add(dir[3]);
 
         dir[4] = new JButton("Up-Left");
         dir[4].setBounds(650, 240, 200, 40);
-        dir[4].setFont(new Font(dir[4].getFont().getName(), dir[4].getFont().getStyle(), 28));
-        add(dir[4]);
 
         dir[5] = new JButton("Up-Right");
         dir[5].setBounds(1100, 240, 200, 40);
-        dir[5].setFont(new Font(dir[5].getFont().getName(), dir[5].getFont().getStyle(), 28));
-        add(dir[5]);
 
         dir[6] = new JButton("Down-Left");
         dir[6].setBounds(650, 360, 200, 40);
-        dir[6].setFont(new Font(dir[6].getFont().getName(), dir[6].getFont().getStyle(), 28));
-        add(dir[6]);
 
         dir[7] = new JButton("Down-Right");
         dir[7].setBounds(1100, 360, 200, 40);
-        dir[7].setFont(new Font(dir[7].getFont().getName(), dir[7].getFont().getStyle(), 28));
-        add(dir[7]);
+
+        for (byte i = 0; i < dir.length; i++) {
+            dir[i].setFont(new Font(dir[i].getFont().getName(), dir[i].getFont().getStyle(), 28));
+            add(dir[i]);
+        }
     }
 
     private void initEvent(){
@@ -113,216 +102,34 @@ public class UI  extends JFrame {
             }
         });
 
-        square[0][0].addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                setButtonSquareBlank();
-                selectedRow = 0;
-                selectedCol = 0;
-                square[0][0].setBackground(Color.RED);
-                square[0][0].setOpaque(true);
+        for (byte row = 0; row < square.length; row++) {
+            for (byte col = 0; col < square[row].length; col++) {
+                byte finalRow = row;
+                byte finalCol = col;
+                square[row][col].addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        setButtonSquareBlank();
+                        selectedRow = finalRow;
+                        selectedCol = finalCol;
+                        square[finalRow][finalCol].setBackground(Color.RED);
+                        square[finalRow][finalCol].setOpaque(true);
+                    }
+                });
             }
-         });
-        square[0][1].addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                setButtonSquareBlank();
-                selectedRow = 0;
-                selectedCol = 1;
-                square[0][1].setBackground(Color.RED);
-                square[0][1].setOpaque(true);
-            }
-        });
-        square[0][2].addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                setButtonSquareBlank();
-                selectedRow = 0;
-                selectedCol = 2;
-                square[0][2].setBackground(Color.RED);
-                square[0][2].setOpaque(true);
-            }
-        });
-        square[0][3].addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                setButtonSquareBlank();
-                selectedRow = 0;
-                selectedCol = 3;
-                square[0][3].setBackground(Color.RED);
-                square[0][3].setOpaque(true);
-            }
-        });
-        square[1][0].addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                setButtonSquareBlank();
-                selectedRow = 1;
-                selectedCol = 0;
-                square[1][0].setBackground(Color.RED);
-                square[1][0].setOpaque(true);
-            }
-        });
-        square[1][1].addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                setButtonSquareBlank();
-                selectedRow = 1;
-                selectedCol = 1;
-                square[1][1].setBackground(Color.RED);
-                square[1][1].setOpaque(true);
-            }
-        });
-        square[1][2].addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                setButtonSquareBlank();
-                selectedRow = 1;
-                selectedCol = 2;
-                square[1][2].setBackground(Color.RED);
-                square[1][2].setOpaque(true);
-            }
-        });
-        square[1][3].addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                setButtonSquareBlank();
-                selectedRow = 1;
-                selectedCol = 3;
-                square[1][3].setBackground(Color.RED);
-                square[1][3].setOpaque(true);
-            }
-        });
-        square[2][0].addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                setButtonSquareBlank();
-                selectedRow = 2;
-                selectedCol = 0;
-                square[2][0].setBackground(Color.RED);
-                square[2][0].setOpaque(true);
-            }
-        });
-        square[2][1].addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                setButtonSquareBlank();
-                selectedRow = 2;
-                selectedCol = 1;
-                square[2][1].setBackground(Color.RED);
-                square[2][1].setOpaque(true);
-            }
-        });
-        square[2][2].addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                setButtonSquareBlank();
-                selectedRow = 2;
-                selectedCol = 2;
-                square[2][2].setBackground(Color.RED);
-                square[2][2].setOpaque(true);
-            }
-        });
-        square[2][3].addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                setButtonSquareBlank();
-                selectedRow = 2;
-                selectedCol = 3;
-                square[2][3].setBackground(Color.RED);
-                square[2][3].setOpaque(true);
-            }
-        });
-        square[3][0].addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                setButtonSquareBlank();
-                selectedRow = 3;
-                selectedCol = 0;
-                square[3][0].setBackground(Color.RED);
-                square[3][0].setOpaque(true);
-            }
-        });
-        square[3][1].addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                setButtonSquareBlank();
-                selectedRow = 3;
-                selectedCol = 1;
-                square[3][1].setBackground(Color.RED);
-                square[3][1].setOpaque(true);
-            }
-        });
-        square[3][2].addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                setButtonSquareBlank();
-                selectedRow = 3;
-                selectedCol = 2;
-                square[3][2].setBackground(Color.RED);
-                square[3][2].setOpaque(true);
-            }
-        });
-        square[3][3].addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                setButtonSquareBlank();
-                selectedRow = 3;
-                selectedCol = 3;
-                square[3][3].setBackground(Color.RED);
-                square[3][3].setOpaque(true);
-            }
-        });
+        }
 
-        dir[0].addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                setButtonDirBlank();
-                selectedDir = 0;
-                dir[0].setBackground(Color.RED);
-                dir[0].setOpaque(true);
-            }
-        });
-        dir[1].addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                setButtonDirBlank();
-                selectedDir = 1;
-                dir[1].setBackground(Color.RED);
-                dir[1].setOpaque(true);
-            }
-        });
-        dir[2].addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                setButtonDirBlank();
-                selectedDir = 2;
-                dir[2].setBackground(Color.RED);
-                dir[2].setOpaque(true);
-            }
-        });
-        dir[3].addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                setButtonDirBlank();
-                selectedDir = 3;
-                dir[3].setBackground(Color.RED);
-                dir[3].setOpaque(true);
-            }
-        });
-        dir[4].addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                setButtonDirBlank();
-                selectedDir = 4;
-                dir[4].setBackground(Color.RED);
-                dir[4].setOpaque(true);
-            }
-        });
-        dir[5].addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                setButtonDirBlank();
-                selectedDir = 5;
-                dir[5].setBackground(Color.RED);
-                dir[5].setOpaque(true);
-            }
-        });
-        dir[6].addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                setButtonDirBlank();
-                selectedDir = 6;
-                dir[6].setBackground(Color.RED);
-                dir[6].setOpaque(true);
-            }
-        });
-        dir[7].addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                setButtonDirBlank();
-                selectedDir = 7;
-                dir[7].setBackground(Color.RED);
-                dir[7].setOpaque(true);
-            }
-        });
-
+        for (byte i = 0; i < dir.length; i++) {
+            byte finalI = i;
+            dir[i].addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    setButtonDirBlank();
+                    selectedDir = finalI;
+                    dir[finalI].setBackground(Color.RED);
+                    dir[finalI].setOpaque(true);
+                }
+            });
+        }
+        
         move.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 // checks if a square and move is selected
