a20d0e147b0d1208504176c1e842ff5e62bb7a58
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2020-11-09 15:59:20 -0500

    rewrite random agent

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
     private Board current;
 
@@ -28,6 +31,26 @@ public class Game {
 
     public boolean isLegal(byte row, byte col, byte dir) {
         return current.validMove(row, col, dir) != 0;
+    }
+
+    public List getLegalMoves() {
+        List<byte[]> moves= new ArrayList<byte[]>();
+
+        for (byte row = 0; row < 4; row++) {
+            for (byte col = 0; col < 4; col++) {
+                for (byte dir = 0; dir < 8; dir++) {
+                    if (isLegal(row, col, dir)) {
+                        byte[] move = new byte[3];
+                        move[0] = row;
+                        move[1] = col;
+                        move[2] = dir;
+                        moves.add(move);
+                    }
+                }
+            }
+        }
+
+        return moves;
     }
 
 }
--- src/RandomAgent.java
+++ src/RandomAgent.java
@@ -1,3 +1,4 @@
+import java.util.List;
 import java.util.Random;
 
 public class RandomAgent {
@@ -15,22 +16,14 @@ public class RandomAgent {
         Random rng = new Random();
 
         System.out.println("RA turn");
-        // Variables to store our randomly generated values
-        byte randomRow, randomCol, randomDir;
-        // TODO: rewrite this using better stuff see channel at 7:31pm 11/8
-        do {
-            // Generate 3 random bytes to use for our move
-            randomRow = (byte) rng.nextInt(4);
-            randomCol = (byte) rng.nextInt(4);
-            randomDir = (byte) rng.nextInt(8);
-            System.out.println("randomRow: " + randomRow + ", randomCol: " + randomCol + ", randomDir: " + randomDir);
-            // If the move is not legal
-            // i.e. it would take us out of bounds, or it uses a square with no stones
-            // or it uses a square with our opponents stones
-        } while (!game.isLegal(randomRow, randomCol, randomDir));
+        List<byte[]> legalMoves = game.getLegalMoves();
+        if (legalMoves.size() < 1) return;
+        System.out.println(legalMoves.size() + " legal moves available for random agent");
+        byte[] chosenMove = legalMoves.get(rng.nextInt(legalMoves.size()));
+        System.out.println(chosenMove[0] + " " + chosenMove[1] + " " + chosenMove[2]);
 
         // Move the game to the next turn
-        game.nextTurn(randomRow, randomCol, randomDir);
+        game.nextTurn(chosenMove[0], chosenMove[1], chosenMove[2]);
 
     }
 }
