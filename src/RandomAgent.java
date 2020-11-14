45233590729e74daad2880d3bb8e8fc6d56a8567
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2020-11-14 12:53:47 -0500

    RandomAgent Comments

--- src/RandomAgent.java
+++ src/RandomAgent.java
@@ -2,32 +2,37 @@ import java.util.List;
 import java.util.Random;
 
 public class RandomAgent {
+    // Our current game & a random number generator we'll use on every move
     private Game game;
     private Random rng;
 
+    /**
+     * Default constructor
+     *
+     * @param game - the game we're using
+     */
     public RandomAgent(Game game) {
         this.game = game;
-        // RNG used for choosing our move
+        // RNG used for choosing our moves
         this.rng = new Random();
-        System.out.println("random agent instantiated");
     }
 
+    /**
+     * Makes a move
+     */
     public void move() {
-        //System.out.println("RA turn");
         // Gets the list of currently legal moves from the game
         List<byte[]> legalMoves = game.getLegalMoves();
-        // If there are no legal moves, don't try to chose one
+        // If there are no legal moves, return
         if (legalMoves.size() < 1) return;
-        //System.out.println(legalMoves.size() + " legal moves available for random agent");
         // Choose a move randomly from the list of legal moves
         byte[] chosenMove = legalMoves.get(rng.nextInt(legalMoves.size()));
-        //System.out.println(chosenMove[0] + " " + chosenMove[1] + " " + chosenMove[2]);
 
         // Move the game to the next turn
         // in getLegalMoves we return a list of byte arrays, which each contain a row, column, and direction
         game.nextTurn(chosenMove[0], chosenMove[1], chosenMove[2]);
     }
-
+    
     public void setGame(Game game) {
         this.game = game;
     }
